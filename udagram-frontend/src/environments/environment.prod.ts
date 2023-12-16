129982be830ad9fcf9f3f539dc272c5dd27e0aae
Maneesh Arora <Maneesh.Arora2@coxautoinc.com>
2023-12-15 20:27:11 -0500

    add screenshots
    added screenshots and deployment changes

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a36bc72c10c6742ddb45f48cab8094cd-393697836.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
